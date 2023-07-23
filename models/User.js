5fdbee9c12cbb3d4502ee306db2f4eed169fbe9f
Manuela-Valencia <valenciamanuela10@gmail.com>
2023-07-22 21:15:15 -0500

    primer crud con node

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../src/utils/connection");
+
+const User = sequelize.define("user", {
+  // users
+
+  first_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  last_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  birthday: {
+    type: DataTypes.DATEONLY,
+    allowNull: false,
+  },
+});
+
+module.exports = User;
